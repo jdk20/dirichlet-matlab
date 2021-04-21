18b523aec299770021613ef87e2113b494cf323e
Jon Kenny <jkenny@caltech.edu>
2021-04-21 16:18:33 -0700

    Added mode and covariance calculations

--- functions/dirstat.m
+++ functions/dirstat.m
@@ -1,13 +1,17 @@
-function [m,v] = dirstat(a)
-%DIRSTAT Mean and variance for the Dirichlet distribution.
-%   [M,V] = DIRSTAT(A) returns the mean and variance of the Dirichlet 
-%   distribution with hyperparameter A. M and V are 1-by-K vectors
-%   where K is the dimensionality of the Dirichlet distribution.
+function [m,mo,v,cv] = dirstat(a)
+%DIRSTAT Mean, mode, variance, and covariance for the Dirichlet distribution.
+%   [M,MO,V,CV] = DIRSTAT(A) returns the mean, mode, variance, and covariance of 
+%   the Dirichlet distribution with hyperparameter A. M, MO, and V are 1-by-K
+%   vectors where K is the dimensionality of the Dirichlet distribution. CV is 
+%   a K-by-K matrix where the diagonal is the variance of A. The Dirichlet
+%   is unimodal for where A > 1.
 %
 %   See also DIRPDF, DIRRND.
     
 %   References:
-%      [1]  A. Gelman, et. al., "Bayesian Data Analysis", CRC Press, 2013
+%      [1] A. Gelman, et. al., "Bayesian Data Analysis", CRC Press, 2013
+%      [2] B. Frigyik, et. al., "Introduction to the Dirichlet Distribution and 
+%          Related Processes", UWEE Technical Report, 2010
 
 
 narginchk(1, 1);
@@ -22,8 +26,26 @@ end
 
 if any(a <= 0)
     m = NaN(1, length(a));
+    mo = NaN(1, length(a));
     v = NaN(1, length(a));
+    cv = NaN(length(a), length(a));
 else
-    m = a./sum(a);
-    v = (m.*(1 - m))/(sum(a) + 1);
+    a0 = sum(a);
+    
+    m = a./a0;
+    mo = (a - 1)./(a0 - length(a));
+    
+    v = (m.*(1 - m))/(a0 + 1);
+    
+    cv = zeros(length(a), length(a));
+    for i = 1:length(a)
+        for j = 1:length(a)
+            if i == j
+                cv(i,j) = a(i)*(a0 - a(i));
+            else
+                cv(i,j) = -a(i)*a(j);
+            end
+        end
+    end
+    cv = cv./(a0^2*(a0 + 1));
 end
